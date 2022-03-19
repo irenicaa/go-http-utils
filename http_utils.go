c43a373744f99d064cf81963ae6093b1b67af950
irenicaa <root@irenica.pro>
2022-03-19 23:48:41 +0300

    Add the response.go file

--- http_utils.go
+++ http_utils.go
@@ -5,13 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net/http"
 )
-
-// Logger ...
-type Logger interface {
-	Print(arguments ...interface{})
-}
 
 // GetJSONData ...
 func GetJSONData(reader io.Reader, data interface{}) error {
@@ -25,34 +19,4 @@ func GetJSONData(reader io.Reader, data interface{}) error {
 	}
 
 	return nil
-}
-
-// HandleError ...
-func HandleError(
-	writer http.ResponseWriter,
-	logger Logger,
-	status int,
-	format string,
-	arguments ...interface{},
-) {
-	message := fmt.Sprintf(format, arguments...)
-	logger.Print(message)
-
-	writer.WriteHeader(status)
-	writer.Write([]byte(message))
-}
-
-// HandleJSON ...
-func HandleJSON(writer http.ResponseWriter, logger Logger, data interface{}) {
-	dataBytes, err := json.Marshal(data)
-	if err != nil {
-		status, message :=
-			http.StatusInternalServerError, "unable to marshal the data: %v"
-		HandleError(writer, logger, status, message, err)
-
-		return
-	}
-
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(dataBytes)
 }
